65149a51c5cc5b559bbf58602d2517c8281f2049
Ismagil Iskakov <absoriann@gmail.com>
2024-09-18 22:29:27 +0300

    fix: allow different origins for devel

--- src/main.py
+++ src/main.py
@@ -4,7 +4,22 @@ from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 from os import environ
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost",
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # db:5432 - service name from compose.yml
 engine = create_engine(
